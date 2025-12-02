1dedae16411b462336099e107c81557e0e593eca
Joao Ricardo <ricardojoao412@gmail.com>
2025-12-02 10:55:02 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("arquivo index.js executado com sucesso!");
+console.log("node nome-da-pasta/arquivo.js roda o arquivo especifico");
+console.log("node --watch nome-da-pasta/arquivo.js fica observando o arquivo para rodar automatico quando salvar"); 
---
+++ src/test.js
@@ -0,0 +1,2 @@
+console.log("esse é o file test.js");
+console.log("esse arquivo e todos os .js ficam na pasta src");
