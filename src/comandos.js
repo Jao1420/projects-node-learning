eca19445990b7165e1c97955a13d6ddac78bf457
Joao Ricardo <ricardojoao412@gmail.com>
2025-12-04 11:02:03 -0400

    adicionando novas funcoes para soma de dados e de rodadas

---
+++ src/comandos.js
@@ -0,0 +1,7 @@
+console.log("CODE abre uma nova guia do visual code");
+console.log("NPM inicia o gerenciador de pacotes Node.js");
+console.log("NPM INIT -Y cria um arquivo package.json com configurações padrão");
+console.log("NPM INSTALL instala as dependências listadas no package.json");
+console.log("NPM INSTALL [nome do pacote] instala um pacote específico");
+console.log("node nome-da-pasta/arquivo.js roda o arquivo especifico");
+console.log("node --watch nome-da-pasta/arquivo.js fica observando o arquivo para rodar automatico quando salvar"); 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,106 @@
-console.log("arquivo index.js executado com sucesso!");
-console.log("node nome-da-pasta/arquivo.js roda o arquivo especifico");
-console.log("node --watch nome-da-pasta/arquivo.js fica observando o arquivo para rodar automatico quando salvar"); 
+// declarando um objeto no javaScripit
+const player1= {
+    NOME: "Mario",
+    VELOCIDADE: 4 ,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+const player2= {
+    NOME: "Luigi",
+    VELOCIDADE: 3 ,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
+const player3= {
+    NOME: "Peach",
+    VELOCIDADE: 3 ,
+    MANOBRABILIDADE: 4,
+    PODER: 2,
+    PONTOS: 0,
+};
+const player4= {
+    NOME: "Yoshi",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 4,
+    PODER: 3,
+    PONTOS: 0,
+};
+const player5= {
+    NOME: "Bowser",
+    VELOCIDADE: 5,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+};
+const player6= {
+    NOME: "Donkey Kong",
+    VELOCIDADE: 2,
+    MANOBRABILIDADE: 2,
+    PODER: 5,
+    PONTOS: 0,
+};
+
+// simular um lançamento de dado 1 até 6
+async function diceRoll(){
+    // aqui iria de 0 até um 1 em decimal 
+return Math.floor(Math.random() * 6) + 1;
+}
+
+// function type of game block
+async function getRandoBlock(){
+    let random= Math.random();
+    let result
+
+    switch(true){
+        case random <0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO"
+            break;
+    }
+    return result;
+}
+
+// function to show the game engine type of blocks and rounds
+async function playRaceEngine(character1, character2){
+
+    for(let round=1; round <=5; round++){
+        console.log("=================================");
+        console.log(`Rodada ${round}`); 
+
+        let block = await getRandoBlock();
+        console.log(`Bloco: ${block}`);
+    }
+
+    let diceResult1= await diceRoll();
+    let diceResult2= await diceRoll();
+
+    let totalSkill1= 0;
+    let totalSkill2= 0;
+    
+    if(block === "RETA"){
+        totalSkill1= diceResult1 + character1.VELOCIDADE;
+        totalSkill2= diceResult2 + character2.VELOCIDADE; 
+    }else if(block === "CURVA"){
+        totalSkill1= diceResult1 + character1.MANOBRABILIDADE;
+        totalSkill2= diceResult2 + character2.MANOBRABILIDADE; 
+    }else{
+
+    }
+
+}
+
+// function main auto invoke
+(async function main(){
+    console.log(`Bem vindo ao jogo de confronto 🏁🚨!`);
+
+    await playRaceEngine(player1, player2);
+    // let jogada= await diceRoll();
+    // console.log(`O jogador tirou ${jogada} no dado!`);
+})();
